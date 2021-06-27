fb7515338f1d106b0596d87cd33da78c25acf324
amalwsl <62916045+amalwsl@users.noreply.github.com>
2021-06-27 01:26:13 +0100

    jsx_project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import imageInSrc from './imageInSrc.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="App"> 
+          <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+              <h1 className="title red">ama wesleti</h1>
+
+              <br/>
+
+              <img src={imageInSrc}  alt="imgsrc" width="20%" />
+
+              <br />
+
+              <img src="/imageInPublic.jpg" alt="imgpublic" width="20%"/>
+
+          </div>
+
+          <video width="320" height="240" controls>
+
+            <source src="myVideo.mp4" type="video/mp4" />
+
+          </video>
     </div>
+    </>
   );
 }
 
